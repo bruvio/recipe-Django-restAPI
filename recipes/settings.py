43562ee3eb270ff755ae988aec3861ddf5156b41
bviola <bruno.viola@ukaea.uk>
2021-10-01 16:27:59 +0100

    improving settings file

--- recipes/settings.py
+++ recipes/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,13 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-fm#c$hng@a&q-_x-)8z1+u))s42r7tf=-g3zb*f$r69*8-@d_e"
+SYSTEM_ENV = config("SYSTEM_ENV")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "*",
+]
 
 
 # Application definition
@@ -75,12 +76,65 @@ WSGI_APPLICATION = "recipes.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "project.db"),
+if SYSTEM_ENV == "PRODUCTION" or SYSTEM_ENV == "STAGING":
+    DEBUG = False
+    SECRET_KEY = config("SECRET_KEY")
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql_psycopg2",
+            "NAME": config("DB_NAME"),
+            "USER": config("POSTGRES_LOCAL_USER"),
+            "PASSWORD": config("POSTGRES_LOCAL_PASSWORD"),
+            "HOST": "db",
+            "PORT": 5432,
+        }
     }
-}
+
+
+elif SYSTEM_ENV == "GITHUB_WORKFLOW":
+    DEBUG = True
+    SECRET_KEY = "TESTING_KEY"
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        }
+    }
+    # DATABASES = {
+    #     "default": {
+    #         "ENGINE": "django.db.backends.postgresql_psycopg2",
+    #         "NAME": "github_actions",
+    #         "USER": "postgres",
+    #         "PASSWORD": "postgres",
+    #         "HOST": "127.0.0.1",
+    #         "PORT": "5432",
+    #     }
+    # }
+
+
+elif SYSTEM_ENV == "DEVELOPMENT":
+    DEBUG = True
+    SECRET_KEY = "DEVELOP_KEY"
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql_psycopg2",
+            "NAME": config("DB_NAME"),
+            "USER": config("POSTGRES_LOCAL_USER"),
+            "PASSWORD": config("POSTGRES_LOCAL_PASSWORD"),
+            "HOST": "localhost",
+            "PORT": "5432",
+        }
+    }
+
+elif SYSTEM_ENV == "NOPOSTGRES":
+    DEBUG = True
+    SECRET_KEY = config("SECRET_KEY")
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        }
+    }
 
 
 # Password validation
