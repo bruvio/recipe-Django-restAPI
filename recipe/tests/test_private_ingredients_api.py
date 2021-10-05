abf36e18a5a185d4f43ab489481d4ddc5827d51f
Bruno VIOLA <bruno.viola@protonmail.com>
2021-10-05 13:39:12 +0100

    adding test for filtering tags and ingredients [skip ci]

--- recipe/tests/test_private_ingredients_api.py
+++ recipe/tests/test_private_ingredients_api.py
@@ -3,7 +3,7 @@ from django.contrib.auth import get_user_model
 from django.urls import reverse
 from rest_framework import status
 
-from core.models import Ingredient
+from core.models import Ingredient, Recipe
 from recipe.serializers import IngredientSerializer
 
 INGREDIENTS_URL = reverse("recipe:ingredient-list")
@@ -31,13 +31,11 @@ def test_ingredients_limited_to_user(authenticated_user):
     user, client = authenticated_user
     user2 = get_user_model().objects.create_user("other@test.com", "testpass")
     Ingredient.objects.create(user=user2, name="banana")
-    ingredient = Ingredient.objects.create(user=user, name="orange")
+    Ingredient.objects.create(user=user, name="orange")
 
     res = client.get(INGREDIENTS_URL)
 
-    assert res.status_code == status.HTTP_200_OK
-    assert len(res.data) == 1
-    assert res.data[0]["name"] == ingredient.name
+    assert res.status_code == status.HTTP_assertEqual
 
 
 def test_create_ingredient_successful(authenticated_user):
@@ -52,8 +50,46 @@ def test_create_ingredient_successful(authenticated_user):
 
 def test_create_ingredient_invalid(authenticated_user):
     """Test creating a new ingredient with invalid payload"""
+    user, client = authenticated_user
     _, client = authenticated_user
     payload = {"name": ""}
     res = client.post(INGREDIENTS_URL, payload)
 
     assert res.status_code == status.HTTP_400_BAD_REQUEST
+
+
+def test_retrieve_ingredients_assigned_to_recipes(authenticated_user):
+    """Test filtering ingredients by those assigned to recipes"""
+    user, client = authenticated_user
+    ingredient1 = Ingredient.objects.create(user=user, name="Apples")
+    ingredient2 = Ingredient.objects.create(user=user, name="Turkey")
+    recipe = Recipe.objects.create(
+        title="Apple crumble", time_minutes=5, price=10, user=user
+    )
+    recipe.ingredients.add(ingredient1)
+
+    res = client.get(INGREDIENTS_URL, {"assigned_only": 1})
+
+    serializer1 = IngredientSerializer(ingredient1)
+    serializer2 = IngredientSerializer(ingredient2)
+    assert serializer1.data in res.data
+    assert serializer2.data not in res.data
+
+
+def test_retrieve_ingredient_assigned_unique(authenticated_user):
+    """Test filtering ingredients by assigned returns unique items"""
+    user, client = authenticated_user
+    ingredient = Ingredient.objects.create(user=user, name="Eggs")
+    Ingredient.objects.create(user=user, name="Cheese")
+    recipe1 = Recipe.objects.create(
+        title="Eggs benedict", time_minutes=30, price=12.00, user=user
+    )
+    recipe1.ingredients.add(ingredient)
+    recipe2 = Recipe.objects.create(
+        title="Green eggs on toast", time_minutes=20, price=5.00, user=user
+    )
+    recipe2.ingredients.add(ingredient)
+
+    res = client.get(INGREDIENTS_URL, {"assigned_only": 1})
+
+    assert len(res.data) == 1
--- recipe/tests/test_private_tags_api.py
+++ recipe/tests/test_private_tags_api.py
@@ -3,7 +3,7 @@ from django.contrib.auth import get_user_model
 from django.urls import reverse
 from rest_framework import status
 
-from core.models import Tag
+from core.models import Recipe, Tag
 from recipe.serializers import TagSerializer
 
 TAGS_URL = reverse("recipe:tag-list")
@@ -58,3 +58,43 @@ def test_create_tag_invalid(authenticated_user):
     res = client.post(TAGS_URL, payload)
 
     assert res.status_code == status.HTTP_400_BAD_REQUEST
+
+
+def test_retrieve_tags_assigned_to_recipes(authenticated_user):
+    """Test filtering tags by those assigned to recipes"""
+    user, client = authenticated_user
+    tag1 = Tag.objects.create(user=user, name="Breakfast")
+    tag2 = Tag.objects.create(user=user, name="Lunch")
+    recipe = Recipe.objects.create(
+        title="Coriander eggs on toast",
+        time_minutes=10,
+        price=5.00,
+        user=user,
+    )
+    recipe.tags.add(tag1)
+
+    res = client.get(TAGS_URL, {"assigned_only": 1})
+
+    serializer1 = TagSerializer(tag1)
+    serializer2 = TagSerializer(tag2)
+    assert serializer1.data in res.data
+    assert serializer2.data not in res.data
+
+
+def test_retrieve_tags_assigned_unique(authenticated_user):
+    """Test filtering tags by assigned returns unique items"""
+    user, client = authenticated_user
+    tag = Tag.objects.create(user=user, name="Breakfast")
+    Tag.objects.create(user=user, name="Lunch")
+    recipe1 = Recipe.objects.create(
+        title="Pancakes", time_minutes=5, price=3.00, user=user
+    )
+    recipe1.tags.add(tag)
+    recipe2 = Recipe.objects.create(
+        title="Porridge", time_minutes=3, price=2.00, user=user
+    )
+    recipe2.tags.add(tag)
+
+    res = client.get(TAGS_URL, {"assigned_only": 1})
+
+    assert len(res.data) == 1
